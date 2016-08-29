1ed1673e79e6af20f98ea82d199eaf5667762868
muzanella <muzanella11@gmail.com>
2016-08-29 20:18:10 +0700

    laratest

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.enem')
+
+@section('content')
+<div class="container">
+    <div class="content">
+        <div class="title">Laravel 5</div>
+        <div class="title">
+            Enem
+        </div>
+    </div>
+</div>
+@endsection()
